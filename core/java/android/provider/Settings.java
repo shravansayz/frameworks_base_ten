dc55235724d120dff31a8578a527b174b8481f5d
SKULSHADY <anushekprasal@gmail.com>
2019-10-10 15:39:20 +0200

    base: Double tap to trigger doze [1/2]
    Thanks to @yank555-lu for the original commit YAOSP-NG/frameworks_base@d6ac164.
    Cleaned up and ported to Android 10 by @SKULSHADY.
    
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>
    Change-Id: If4bcca326eb8af168f2b63a4a07bf3350f3c3030

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7171,6 +7171,12 @@ public final class Settings {
         public static final String DOZE_BRIGHTNESS_FORCE = "doze_brightness_force";
 
         /**
+         * Use doubletap as doze pulse triggers
+         * @hide
+         */
+        public static final String DOZE_TRIGGER_DOUBLETAP = "doze_trigger_doubletap";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
