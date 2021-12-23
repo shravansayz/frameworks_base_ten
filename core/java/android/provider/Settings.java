ac34ebb8085c49d80f6ae72531c18fdfc72bff15
Ido Ben-Hur <idoybh2@gmail.com>
2021-12-23 17:24:28 +0200

    SystemUI: Add a toggle for secure tiles on keyguard [1/2]
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -14039,6 +14039,13 @@ public final class Settings {
         public static final String EXTRA_DIM_AUTO_TIME = "extra_dim_auto_time";
         
         /**
+         * Whether to show QS auto brightness toggle button
+         * @hide
+         */
+        @Readable
+        public static final String QSTILE_REQUIRES_UNLOCKING = "qstile_requires_unlocking";
+
+        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */
