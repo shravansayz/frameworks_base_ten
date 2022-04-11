de66378b3a2f43c2e5aed01a4dd44519d75b86ae
cjh1249131356 <cjh1249131356@gmail.com>
2022-04-11 19:41:45 +0800

    SystemUI: Introduce preferred network tile
    **Adapt to Android 14 Matrixx
    
    Change-Id: I673cdc073c50dbcb9197edd663855dd78a187d93
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    Signed-off-by: mukesh22584 <mks22584@gmail.com>
    Co-Authored-By: Roxor-007 <koyande72@gmail.com>
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

--- packages/SystemUI/src/com/android/systemui/lineage/LineageModule.kt
+++ packages/SystemUI/src/com/android/systemui/lineage/LineageModule.kt
@@ -30,6 +30,7 @@ import com.android.systemui.qs.tiles.SyncTile
 import com.android.systemui.qs.tiles.UsbTetherTile
 import com.android.systemui.qs.tiles.VpnTile
 import com.android.systemui.qs.tiles.WifiTile
+import com.android.systemui.qs.tiles.PreferredNetworkTile
 
 import dagger.Binds
 import dagger.Module
@@ -115,4 +116,10 @@ interface LineageModule {
     @IntoMap
     @StringKey(WifiTile.TILE_SPEC)
     fun bindWifiTile(wifiTile: WifiTile): QSTileImpl<*>
+
+    /** Inject PreferredNetworkTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(PreferredNetworkTile.TILE_SPEC)
+    fun bindPreferredNetworkTile(preferredNetworkTile: PreferredNetworkTile): QSTileImpl<*>
 }
