d0a78c8990ecf726eb70c23fdec2adb9008fa362
maxwen <max.weninger@gmail.com>
2024-08-17 19:55:57 +0530

    SystemUI: [SQUASHED] Doze custom brightness config [1/2]
    SystemUI: Add separate config for pulse brightness [1/2]
    config_screenBrightnessPulse
    if value is -1 it is usiing config_screenBrightnessDoze
    
    SystemUI: Custom config to force custom doze brightness [1/2]
    dont clamp the doze or pulze brightness against the current screen
    brightness. Cause there might be actually a reason why you
    did set a custom value in the first place
    
    - Aston-Martinn: Bring it on A14
    
    Change-Id: I11a54e57b586e7919f9681d16219c5e47f3bea02
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7154,6 +7154,23 @@ public final class Settings {
         public static final String CUSTOM_AMBIENT_POCKETMODE_GESTURE = "custom_ambient_pocketmode_gesture";
 
         /**
+         * User definable value of pulse notification screen brightness
+         * @hide
+         */
+        public static final String PULSE_BRIGHTNESS = "pulse_brightness";
+
+        /**
+         * User definable value of aod screen brightness
+         * @hide
+         */
+        public static final String DOZE_BRIGHTNESS = "doze_brightness";
+
+        /**
+         * @hide
+         */
+        public static final String DOZE_BRIGHTNESS_FORCE = "doze_brightness_force";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
