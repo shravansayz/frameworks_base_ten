ProjectEverest-staging/frameworks_base

SystemUI: Use VibrationUtils for QS/Status bar haptics
* VibrationsUtils uses single thread executor compared to
  vibratorhelper's performHapticFeedback,single thread seems to play nicer
  with animations compared to async/normal vibration callbacks based from our tests.
  We initially observed this when upstreaming VibrationUtils coming from async tasks
  where we observed hiccups/lags when triggering vibrations
  for edge back gesture animation.

test: swipe QS panel, no hiccups observed when expanding QS with media player
which is better compared to performHapticFeedback where we observed a bit
of hiccups especially when qs media player is visible

Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -244,6 +244,7 @@ import com.android.systemui.util.time.SystemClock;
 import com.android.wm.shell.animation.FlingAnimationUtils;
 
 import android.util.RisingBoostFramework;
+import com.android.internal.util.android.VibrationUtils;
 
 import dalvik.annotation.optimization.NeverCompile;
 import com.android.systemui.aospa.NotificationLightsView;
@@ -3860,10 +3861,7 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
     private void maybeVibrateOnOpening(boolean openingWithTouch) {
         if (mVibrateOnOpening && mBarState != KEYGUARD && mBarState != SHADE_LOCKED) {
             if (!openingWithTouch || !mHasVibratedOnOpen) {
-                mVibratorHelper.performHapticFeedback(
-                        mView,
-                        HapticFeedbackConstants.GESTURE_START
-                );
+                VibrationUtils.triggerVibration(mView.getContext(), 2);
                 mHasVibratedOnOpen = true;
                 mShadeLog.v("Vibrating on opening, mHasVibratedOnOpen=true");
             }

packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesCommandQueueCallbacks.java
@@ -67,6 +67,8 @@ import com.android.systemui.statusbar.policy.KeyguardStateController;
 import com.android.systemui.statusbar.policy.RemoteInputQuickSettingsDisabler;
 import com.android.systemui.statusbar.policy.SecureLockscreenQSDisabler;
 
+import com.android.internal.util.android.VibrationUtils;
+
 import dagger.Lazy;
 
 import java.util.Arrays;
@@ -312,7 +314,7 @@ public class CentralSurfacesCommandQueueCallbacks implements CommandQueue.Callba
             mMetricsLogger.action(MetricsEvent.ACTION_SYSTEM_NAVIGATION_KEY_DOWN);
             if (mShadeViewController.isFullyCollapsed()) {
                 if (mVibrateOnOpening) {
-                    vibrateOnNavigationKeyDown();
+                    VibrationUtils.triggerVibration(mContext, 2);
                 }
                 mShadeViewController.expand(true /* animate */);
                 mNotificationStackScrollLayoutController.setWillExpand(true);

shravansayz/frameworks_base_ten

Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -238,6 +238,8 @@ import com.android.wm.shell.animation.FlingAnimationUtils;
 
 import lineageos.providers.LineageSettings;
 
+import com.android.internal.util.tenx.VibrationUtils;
+
 import dalvik.annotation.optimization.NeverCompile;
 
 import kotlin.Unit;
@@ -3772,10 +3774,7 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
     private void maybeVibrateOnOpening(boolean openingWithTouch) {
         if (mVibrateOnOpening && mBarState != KEYGUARD && mBarState != SHADE_LOCKED) {
             if (!openingWithTouch || !mHasVibratedOnOpen) {
-                mVibratorHelper.performHapticFeedback(
-                        mView,
-                        HapticFeedbackConstants.GESTURE_START
-                );
+                VibrationUtils.triggerVibration(mView.getContext(), 2);
                 mHasVibratedOnOpen = true;
                 mShadeLog.v("Vibrating on opening, mHasVibratedOnOpen=true");
             }

packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesCommandQueueCallbacks.java
@@ -66,6 +66,8 @@ import com.android.systemui.statusbar.policy.KeyguardStateController;
 import com.android.systemui.statusbar.policy.RemoteInputQuickSettingsDisabler;
 import com.android.systemui.statusbar.policy.SecureLockscreenQSDisabler;
 
+import com.android.internal.util.tenx.VibrationUtils;
+
 import dagger.Lazy;
 
 import java.util.Arrays;
@@ -313,7 +315,7 @@ public class CentralSurfacesCommandQueueCallbacks implements CommandQueue.Callba
             mMetricsLogger.action(MetricsEvent.ACTION_SYSTEM_NAVIGATION_KEY_DOWN);
             if (mShadeViewController.isFullyCollapsed()) {
                 if (mVibrateOnOpening) {
-                    vibrateOnNavigationKeyDown();
+                    VibrationUtils.triggerVibration(mContext, 2);
                 }
                 mShadeController.animateExpandShade();
                 mNotificationStackScrollLayoutController.setWillExpand(true);